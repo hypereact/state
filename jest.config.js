c5d98135e8108f3dcc9e9a67659906bcdd1c635c
Marco Minetti <marco.minetti@gmail.com>
2021-01-04 00:30:47 +0100

    updated jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
   collectCoverage: true,
   coverageDirectory: "./build/coverage",
   coverageReporters: ["json"]
